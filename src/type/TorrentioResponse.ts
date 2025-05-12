41b238194375acfd03ce86d338c202f6434cfcaf
Ruben Lousada <ruben.lousada.dev@gmail.com>
2025-05-12 23:40:38 +0100

    install axios

---
+++ src/type/TorrentioResponse.ts
@@ -0,0 +1,20 @@
+export interface TorrentioResponse {
+    streams: Stream[];
+    "cacheMaxAge": number,
+    "staleRevalidate": number,
+    "staleError": number
+}
+
+export interface BehaviourHint {
+    bingeGroup: string;
+    filename: string;
+}
+
+export interface Stream {
+    name: string,
+    title: string,
+    infoHash: string,
+    fileIdx: number,
+    behaviorHints: BehaviourHint,
+    sources?: string[]
+}
