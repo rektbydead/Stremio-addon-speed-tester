69587ffab16ba1d339f19c7dded4a3bd6893f50b
Ruben Lousada <ruben.lousada.dev@gmail.com>
2025-05-13 19:15:18 +0100

    Use best trackers

--- src/utils/TrackerObtainer.ts
+++ src/utils/TrackerObtainer.ts
@@ -1,16 +1,21 @@
 const TRACKERS: string[] = []
 
-export async function getTrackers(){
+export async function getTrackers(): Promise<string[]> {
     if (TRACKERS.length > 0) {
         return TRACKERS
     }
 
-    const response = await fetch("https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt")
+    const response = await fetch("https://raw.githubusercontent.com/ngosang/trackerslist/refs/heads/master/trackers_best.txt")
     const text = await response.text()
     const trackerList: string[] = text.split('\n')
         .filter(t => t.trim() && !t.startsWith('udp://'))
-        .slice(0, 15)
 
     TRACKERS.push(...trackerList)
     return TRACKERS
+}
+
+export function getTrackerFromSources(sources: string[]): string[] {
+    return sources
+        .map(source => source.replace("tracker:", ""))
+        .filter(source => source.trim() && !source.startsWith('udp://'))
 }
